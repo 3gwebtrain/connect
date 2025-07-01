a1776bc1e137f36858b22b7d8bbefd9d2841ccb7
3gwebtrain <3gwebtrain@gmail.com>
2025-07-01 10:19:05 +0530

    reset style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App():React.ReactElement {
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        Welcome
       </div>
     </>
   )
