4e6471e1a1821a208aa9aaaf9a94d888b477d876
3gwebtrain <3gwebtrain@gmail.com>
2025-07-01 08:56:42 +0530

    initial commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import App from './App';
+describe('App', () => {
+    it('renders the Vite + React logo', () => {
+      render(<App />);
+    });
+  });
